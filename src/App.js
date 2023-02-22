bafcfc1393ece3b7bec81c9bd3a34b0a9eed02c9
Ahmed_Samy <ahmed.samy1640@gmail.com>
2023-02-22 08:54:37 +0200

    Project 2 completed

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import './App.css';
+import JSONDATA from "./MOCK_DATA.json"
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+  return (
+    <div className="App">
+      <input type="text" placeholder="search..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      {
+        JSONDATA.filter((val) => {
+          if (searchTerm === "") return val
+          else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) return val
+          return null
+        }).map((val, key) => {
+          return <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
